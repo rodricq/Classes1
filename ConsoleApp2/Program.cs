b97d4aec71cf3dbdb55a62042c87f165855f8bee
beverley-dick <beverleydick11@gmail.com>
2023-05-08 16:56:35 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Tutorial
+{
+    public static void Main()
+    {
+        // write your code here
+        string productName = "TV";
+        int productYear = 2023;
+        float productPrice = 279.99f;
+        string myFirstName = "Beverley";
+        string myLastName = "Dick";
+        int myAge = 22;
+        string myProgramme = "computer Engineering";
+
+        //test code
+        Console.WriteLine("productName: " + productName);
+        Console.WriteLine("productYear: " + productYear);
+        Console.WriteLine("productPrice: " + productPrice);
+        Console.WriteLine("myFirstame: " + myFirstName);
+        Console.WriteLine("myLastName: " + myLastName);
+        Console.WriteLine("myAge: " + myAge);
+        Console.WriteLine("myProgramme: " + myProgramme);
+
+    }
+    }
